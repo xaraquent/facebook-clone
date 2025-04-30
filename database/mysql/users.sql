2c4a35203a6189df23fdb2d8f94e9c3ba0fac054
HampusLarss0n <hlarsson0616@gmail.com>
2025-04-30 11:45:23 +0200

    wopps

--- database/mysql/users.sql
+++ database/mysql/users.sql
@@ -10,10 +10,3 @@ CREATE TABLE Users (
     FOREIGN KEY (user_group_id) REFERENCES Groups_chat(group_id)
 );
 
-INSERT INTO Users(user_name, user_password, user_email) VALUES 
-('Hampus123', 'KaKa123!', 'hampus@gmail.com'), 
-('CatLOVER', 'Cat20love*', 'cat@gmail.com'),
-('xX_Hate_Xx', 'Hate!123', 'hate@gmail.com');
-
-SELECT COUNT(user_id)
-FROM Users;
