1586781269869ec55f071031247eaf881a0ce75b
HampusLarss0n <hlarsson0616@gmail.com>
2025-04-30 11:31:56 +0200

    removd Not null on post_reaction

--- database/mysql/posts.sql
+++ database/mysql/posts.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Posts(
     post_id INT AUTO_INCREMENT NOT NULL,
     post_user_id INT,
-    post_reaction VARCHAR(255) NOT NULL,
+    post_reaction VARCHAR(255),
     post_content VARCHAR(1000) NOT NULL,
     PRIMARY KEY (post_id),
     FOREIGN KEY (post_user_id) REFERENCES Users(user_id)
