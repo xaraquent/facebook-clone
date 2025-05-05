c3fba3f2d5852d01459c84be7a75d7749b3ffb27
Will Axelsson <will.axelsson@iths.se>
2025-05-05 09:09:45 +0200

    Added app.js and routes for friends

---
+++ backend/services/friendsService.js
@@ -0,0 +1,31 @@
+const connectionMySQL = require('../connectionMySQL');
+
+function getFriends() {
+  return new Promise((resolve, reject) => {
+    let sql = 'SELECT * FROM Friends';
+    connectionMySQL.query(sql, (err, rows) => {
+      if (err) reject(err);
+      else resolve(rows);
+    });
+  });
+}
+
+function friendsAndUsers() {
+  return new Promise((resolve, reject) => {
+    let sql = `
+    Users.user_name, Friends.friend_name
+    FROM Friend_list
+    INNER JOIN Users ON Friend_list.friend_user_ID = Users.user_ID
+    INNER JOIN Friends On Friend_list.friend_friend_ID = Friends.friend_ID
+    `;
+    connectionMySQL.query(sql, (err, rows) => {
+      if (err) reject(err);
+      else resolve(rows);
+    });
+  });
+}
+
+module.exports = {
+  getFriends,
+  friendsAndUsers,
+};
