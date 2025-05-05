9bd2d1323012623f8764de99cb35e2c217d266f0
Michelle Sturesson - JSU24 GBG <michelle.sturesson@iths.se>
2025-05-05 23:34:58 +0200

    la till userID i comment tabell och skapade mappar i backend

--- database/mysql/comments.sql
+++ database/mysql/comments.sql
@@ -1,8 +1,10 @@
 CREATE TABLE Comments(
     comment_id INT AUTO_INCREMENT NOT NULL,
     comment_post_id INT,
+    comment_user_id INT,
     comment_reaction VARCHAR(255),
     comment_content VARCHAR(255),
     PRIMARY KEY (comment_id),
     FOREIGN KEY (comment_post_id) REFERENCES Posts(post_id)
+    FOREIGN KEY (comment_user_id) REFERENCES Users(user_id)
 );
