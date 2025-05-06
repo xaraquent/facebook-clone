797c10bde0ebcaae47ddcd6ef6965873e92e6619
HampusLarss0n <hlarsson0616@gmail.com>
2025-05-06 09:23:50 +0200

    added user rout/controller/Service

---
+++ backend/controllers/usersController.js
@@ -0,0 +1,12 @@
+const usersService = require('../services/')
+
+exports.getUsers = async (req, res) => {
+  try{
+    const users = await usersService.getUsers();
+    res.json({users});
+  } catch (error){
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+};
