6fdf85106a5b7a36b021072f39da9ad5793901bc
xaraquent <simon.michael.te15d@gmail.com>
2025-05-07 07:15:42 +0200

    typo

--- database/mysql/comments.sql
+++ database/mysql/comments.sql
@@ -5,6 +5,6 @@ CREATE TABLE Comments(
     comment_reaction VARCHAR(255),
     comment_content VARCHAR(255),
     PRIMARY KEY (comment_id),
-    FOREIGN KEY (comment_post_id) REFERENCES Posts(post_id)
+    FOREIGN KEY (comment_post_id) REFERENCES Posts(post_id),
     FOREIGN KEY (comment_user_id) REFERENCES Users(user_id)
 );
