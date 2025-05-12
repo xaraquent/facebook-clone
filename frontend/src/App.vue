d1b25a0a979f243d994ce12e25160469b974f60c
Will Axelsson <will.axelsson@iths.se>
2025-05-12 11:23:46 +0200

    Merged and fixed homeview in routes

---
+++ frontend/src/App.vue
@@ -0,0 +1,6 @@
+<script setup>
+import { RouterView, RouterLink } from 'vue-router';
+</script>
+<template>
+  <RouterView></RouterView>
+</template>
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import LoginView from './views/loginview.vue';
+import homeview from './views/homeview.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: LoginView,
+    },
+    {
+      path: '/',
+      name: 'home',
+      component: homeview,
     },
   ],
 });
