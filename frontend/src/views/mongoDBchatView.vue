d91849692ebc6e1aaffd69acb854b23158c92854
Will Axelsson <will.axelsson@iths.se>
2025-05-16 10:47:58 +0200

    Showing the messages in frontend

--- frontend/src/views/mongoDBchatView.vue
+++ frontend/src/views/mongoDBchatView.vue
@@ -1,14 +1,21 @@
 <script setup>
 import { ref } from 'vue';
+
+const numberOfMessages = ref('');
+const messages = ref([]);
+
 fetch('http://localhost:3000/facebook/messages')
   .then((res) => res.json())
   .then((data) => {
     console.log(data);
+    numberOfMessages.value = data.length;
+    messages.value = data;
   });
 </script>
 
 <template>
   <section class="chat-container">
-    <h1>hej</h1>
+    <p>Antal meddelande: {{ numberOfMessages }}</p>
+    <p v-for="message in messages" :key="message._id">{{ message.message_content }}</p>
   </section>
 </template>
