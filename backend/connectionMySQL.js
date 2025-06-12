aa266002a379e6ee36b1e4e0f4a27b0635367ee3
Simon <simon.michael.te15d@gmail.com>
2025-06-12 11:05:44 +0200

    error handling

--- backend/connectionMySQL.js
+++ backend/connectionMySQL.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
+console.log('Användare från .env:', process.env.MYSQL_USER);
+
 const connectionMySQL = mysql.createConnection({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
@@ -10,6 +12,13 @@ const connectionMySQL = mysql.createConnection({
 });
 
 connectionMySQL.connect((err) => {
+  console.log({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT,
+  });
   if (err) {
     console.error('❌ Error connecting to database:', err);
   } else {
