b8d3adba80af90ef3779b501a45ab762824d59be
xaraquent <simon.michael.te15d@gmail.com>
2025-06-12 23:13:01 +0200

    reverted database name change

--- backend/connectionMongoDB.js
+++ backend/connectionMongoDB.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectionMongoDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/facebook_clone');
+    await mongoose.connect('mongodb://127.0.0.1:27017/facebookClone');
   } catch (error) {
     console.error(error);
     process.exit(1);
