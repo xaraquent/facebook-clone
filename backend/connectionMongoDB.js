8c52849e80e9b6e6c85770c8193b3d2bf026fd1b
xaraquent <simon.michael.te15d@gmail.com>
2025-06-12 22:58:23 +0200

    database name change

--- backend/connectionMongoDB.js
+++ backend/connectionMongoDB.js
@@ -1,14 +1,12 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const connectionMongoDB = async () => {
-    try {
-        await mongoose.connect(
-            "mongodb://127.0.0.1:27017/facebookClone"
-        )
-    } catch (error) {
-        console.error(error);
-        process.exit(1);
-    }
-}
+  try {
+    await mongoose.connect('mongodb://127.0.0.1:27017/facebook_clone');
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
 
 module.exports = connectionMongoDB;
